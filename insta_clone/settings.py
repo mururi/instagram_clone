af6ebc6f7f399849d74391d9c249fecb39c301c1
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-04 18:47:19 +0300

    Setup navbar, basefile and static files

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -138,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
