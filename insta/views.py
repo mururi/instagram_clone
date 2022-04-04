1d449b1f36cf017b5dbef37a911e6c6225b72c66
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-04 22:14:18 +0300

    Add login page

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Image
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url = 'accounts/login/')
 def home(request):
     images = Image.get_images()
     return render(request, 'index.html', {"images": images})
