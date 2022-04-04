1d536bc4fcc2860e9df4442667df1430e8cadc84
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-04 15:47:22 +0300

    Add base.html

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 def home(request):
-    return render(request, 'index.html')
+    images = Image.get_images()
+    return render(request, 'index.html', {"images": images})
