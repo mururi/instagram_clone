801eea642b94d31c82abb3052ff5a6a17256a833
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-04 20:23:05 +0300

    Add registration page and setup django_registration

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('insta.urls'))
+    path('', include('insta.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
