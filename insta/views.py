ffaea2a32d99831f171e610450ad365587fed4d2
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-05 18:56:07 +0300

    Add email field to registration form

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Image
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 @login_required(login_url = 'accounts/login/')
 def home(request):
@@ -11,5 +12,12 @@ def home(request):
     return render(request, 'index.html', {"images": images})
 
 def register(request):
-    form = UserCreationForm
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            return redirect('home')
+    else:
+        form = RegisterForm()
     return render(request, 'registration/register.html', {"form": form})
