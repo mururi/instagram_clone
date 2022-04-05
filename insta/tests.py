1688e81e991bb5841a63527cc46eeda28ecd9756
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-06 01:25:17 +0300

    Add test profile instance method

--- insta/tests.py
+++ insta/tests.py
@@ -1,5 +1,30 @@
 from django.test import TestCase
 from .models import Profile, Image, Comment
+from django.contrib.auth.models import User
 
 class ProfileTestClass(TestCase):
-    pass
+    '''
+    Test class to test the behaviour of the Profile model class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test case
+        '''
+       
+        # Creating a new user
+        self.new_user = User(username = 'Doe', password = 'pass', email = 'johndoe@company.com')
+        self.new_user.save()
+
+        # Creating a new profile
+        self.new_profile = Profile(id = 1, user = self.new_user, bio = 'Test short bio')
+        self.new_profile.save()
+
+    def test_instance(self):
+        '''
+        Method to test if the new_profile object is an instance of the Profile model
+        '''
+
+        self.assertTrue(self.new_profile, Profile)
+
+    
