45d55afa4badbc7adbc78f8f8a03ebe6bca207b1
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-05 18:19:27 +0300

    Add new registration form

--- insta/views.py
+++ insta/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Image
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required(login_url = 'accounts/login/')
 def home(request):
@@ -9,4 +11,5 @@ def home(request):
     return render(request, 'index.html', {"images": images})
 
 def register(request):
-    pass
+    form = UserCreationForm
+    return render(request, 'registration/register.html', {"form": form})
